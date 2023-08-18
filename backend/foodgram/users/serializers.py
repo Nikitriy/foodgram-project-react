49785b30fe11d15e0ac282e0ec8926b5e3a29a40
Никита Волков <iallknow1@yandex.ru>
2023-08-18 15:30:39 +0300

    Fix authorithation

--- backend/foodgram/users/serializers.py
+++ backend/foodgram/users/serializers.py
@@ -22,7 +22,7 @@ class CustomUserCreateSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         validated_data['password'] = make_password(validated_data['password'])
-        return CustomUser.objects.create(validated_data)
+        return super().create(validated_data)
 
 
 class CustomUserSerializer(serializers.ModelSerializer):
